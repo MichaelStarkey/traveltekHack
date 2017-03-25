9f07383943e340a771180c399ff7a28860aac8af
Michael Starkey <2194203s@student.gla.ac.uk>
2017-03-25 11:34:30 +0000

    Update website.py

--- website.py
+++ website.py
@@ -4,7 +4,7 @@ import sqlite3
 
 @route('/')
 def main():
-    return template('<b>Hello world!</b>!')
+    return '<b>Hello world!</b>!'
 
 # runs the full website
 run(host='0.0.0.0', port=80, debug=True)
