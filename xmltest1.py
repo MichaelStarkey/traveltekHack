5e730b9e41b9a7c431b40a9f2acb425b11422750
root <root@Micks-T510.home>
2017-03-25 12:41:22 +0000

    xml

--- xmltest1.py
+++ xmltest1.py
@@ -5,19 +5,21 @@ from lxml import etree
 
 testreq = '''<?xml version="1.0"?>
 <request>
-	<auth username="hackathon" password="pr38ns48" />
-	<method action="simplesearch" sitename="cruisedemo.traveltek.net"status="Live" type="cruise">
-		<searchdetail type="cruise" startdate="2017-04-01" enddate="2017-04-30"adults="2" children="0" sid="30115" resultkey="default">
-		</searchdetail>
-	</method>
+<auth username="hackathon" password="pr38ns48" />
+<method action="simplesearch" sitename="cruisedemo.traveltek.net" status="Live" type="cruise">
+<searchdetail type="cruise" startdate="2017-04-01" enddate="2017-04-30" adults="2" children="0" sid="30115" resultkey="default">
+</searchdetail>
+</method>
 </request>'''
 
 print(testreq)
 
 # make the post request to the api url
-r = requests.post('https://fusionapi.traveltek.net/0.9/interface.pl', data={"xml": testreq})
+r = requests.post('https://fusionapi.traveltek.net/0.9/interface.pl', 
+data={"xml": testreq})
 # parse the response
 root = etree.fromstring(r.text)
+print (r.text)
 # loop through it and print the hotel name of each result
 for element in root.iterfind("results/cruise"): 
 	name=element.get("name") 
